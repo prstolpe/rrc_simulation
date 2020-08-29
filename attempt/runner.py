fa58150b10b29aa3e83c1e73a8edbafbe3e971b1
prstolpe <pr.stolpe@gmail.com>
2020-08-29 09:38:48 +0200

    Execution file for FetchReach plus visualization

---
+++ attempt/runner.py
@@ -0,0 +1,26 @@
+
+from attempt.ddpg import HERDDPG, DDPG
+import gym
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+if __name__ == "__main__":
+
+
+    env = gym.make('FetchReach-v1')
+    agent = HERDDPG(env)
+    agent.drill()
+
+    plt.plot(np.vstack(agent.rewards))
+    plt.title('Rewards')
+    plt.show()
+
+    plt.plot(np.vstack(agent.policy_losses))
+    plt.title('Policy Losses')
+    plt.show()
+
+    plt.plot(np.vstack(agent.value_losses))
+    plt.title('Value Losses')
+    plt.show()
